a9975f4d0bd969ee81b31ed480c96f5c7a44e4f9
Keiichi Hoshina <masayukihoshina3@gmail.com>
2021-08-03 00:42:59 +0900

    36. タスクの戻す機能

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+  createIncomleteList(inputText);
+};
 
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncomleteList = (text) => {
   // li要素生成
   const li = document.createElement("li");
   li.className = "list-row";
 
   // div要素生成
   const div = document.createElement("div");
-  div.innerText = inputText;
+  div.innerText = text;
 
   // button（完了）要素生成
   const completButton = document.createElement("button");
@@ -25,6 +34,12 @@ const onClickAdd = () => {
     li.innerText = text;
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const deletTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deletTarget);
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncomleteList(text);
+    });
     addTarget.appendChild(div);
     addTarget.appendChild(backButton);
     // 完了リストに追加
@@ -45,11 +60,6 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
